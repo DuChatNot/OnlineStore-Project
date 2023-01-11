b427ded7a65dbf47c22bbb2dfc1f0fdf5371e418
duChat <cpantoja022@gmail.com>
2023-01-10 18:16:12 -0600

    Mobile menu + Desktop menu combined, medias added too

--- JSmain.js
+++ JSmain.js
@@ -1,8 +1,15 @@
 const navE = document.querySelector('.navbar-email');
 const menu = document.querySelector('.desktop-menu');
+const bicon = document.querySelector('.menu');
+const mobmenu = document.querySelector('.mobile-menu');
 
 navE.addEventListener('click', toggleMenu);
+bicon.addEventListener('click', toggleMobMenu);
 
 function toggleMenu() {
     menu.classList.toggle('inactive');
+}
+
+function toggleMobMenu() {
+    mobmenu.classList.toggle('inactive');
 }
