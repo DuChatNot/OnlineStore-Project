b57388672b90a19e05e5b96c38ad8ef38c887ea6
duChat <cpantoja022@gmail.com>
2023-01-11 19:45:03 -0600

    Main Interactions & Menus fully working

--- JSmain.js
+++ JSmain.js
@@ -1,3 +1,4 @@
+//Constantes globales
 const navE = document.querySelector('.navbar-email');
 const menu = document.querySelector('.desktop-menu');
 const bicon = document.querySelector('.menu');
@@ -6,6 +7,7 @@ const cart = document.querySelector('.product-detail');
 const mobmenu = document.querySelector('.mobile-menu');
 const cardsContainer = document.querySelector('.cards-container');
 
+//Eventos & Variables útiles
 navE.addEventListener('click', toggleMenu);
 bicon.addEventListener('click', toggleMobMenu);
 carticon.addEventListener('click', shcrt);
@@ -14,7 +16,7 @@ const mobMenuOpen = mobmenu.classList.contains('inactive');
 const menuOpen = menu.classList.contains('inactive');
 const cartOpen = cart.classList.contains('inactive');
 
-
+// Open/Close Functions
 function toggleMenu() { /* Menu pequeño */
     menu.classList.toggle('inactive');
     if(cartOpen){
@@ -48,6 +50,8 @@ function shcrt(){ /*Menu lateral*/
     }
 }
 
+//Array & Products ///////////////////////////////////////////////////////////////////
+
 const plist = []; //El arrays sera tan largo como se le agregen elementos (.push)
 
 plist.push({
@@ -65,32 +69,56 @@ plist.push({
     price: 20,
     image: 'https://images.pexels.com/photos/8744822/pexels-photo-8744822.jpeg?auto=compress&cs=tinysrgb&w=1600',
 });
+plist.push({
+    name: 'Dummy',
+    price: 500,
+    image: 'https://images.pexels.com/photos/6296027/pexels-photo-6296027.jpeg?auto=compress&cs=tinysrgb&w=1600',
+});
+plist.push({
+    name: 'Pads',
+    price: 40,
+    image: 'https://images.pexels.com/photos/6296119/pexels-photo-6296119.jpeg?auto=compress&cs=tinysrgb&w=1600',
+});
+plist.push({
+    name: 'Bandages',
+    price: 10,
+    image: 'https://images.pexels.com/photos/9943942/pexels-photo-9943942.jpeg?auto=compress&cs=tinysrgb&w=1600',
+});
+
+//Constantes Aside lado B/////////////////////////////////////////////////////////////////////////////////////
 
 const productDetail = document.querySelector('.product-detail-B');
 const productDetailOpen = productDetail.classList.contains('inactive');
 
-/*Función creadora*/ function renderObjects(arr){
+const close = document.querySelector('.product-detail-B-close');
+close.addEventListener('click',f)
+
+function f(){
+    productDetail.classList.toggle('inactive');
+
+    if (mobMenuOpen){
+        mobmenu.classList.add('inactive');
+    } 
+    
+    if(cartOpen){ 
+        cart.classList.add('inactive');
+    }
+
+    if(menuOpen){
+        menu.classList.add('inactive');
+    }
+}
+
+
+//Función creadora///////////////////////////////////////////////////////////////
+
+function renderObjects(arr){
     for (product of arr){ //Para cada variable del arreglo:
 
         const productCard = document.createElement('div'); //Se crea sección Div
         productCard.classList.add('product-card'); //Clase de sección Div
         productCard.addEventListener('click',f);
-        function f(){
-            productDetail.classList.toggle('inactive');
-
-            if (mobMenuOpen){
-                mobmenu.classList.add('inactive');
-            } 
-            
-            if(cartOpen){ /*????*/ 
-                cart.classList.add('inactive');
-            }
-
-            if(menuOpen){
-                menu.classList.add('inactive');
-            }
-
-        }
+        
 
         const img = document.createElement('img'); //Se crea imagen
         img.setAttribute('src', product.image); //Se obtiene y otorga la fuente de la imagen
